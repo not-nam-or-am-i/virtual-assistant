64d3665245166809e61f99ac0a7df13721d60dc5
Nguyen Thanh Nam <namnt4499@gmail.com>
2020-07-02 23:47:29 +0700

    thêm try except cho wiki

--- wiki.py
+++ wiki.py
@@ -3,4 +3,7 @@ import wikipedia
 def answer_wiki(text):
     #get query string
     wikipedia.set_lang('vi')
-    return wikipedia.summary(text, sentences=1)
+    try:
+        return wikipedia.summary(text, sentences=1)
+    except:
+        return 'Jarvis không biết đáp án của câu hỏi'
