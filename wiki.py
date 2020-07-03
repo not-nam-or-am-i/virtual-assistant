12168eea6566e1174996e0506df18588d119b15c
Nguyen Thanh Nam <namnt4499@gmail.com>
2020-07-03 10:18:57 +0700

    update wiki search

--- wiki.py
+++ wiki.py
@@ -2,8 +2,15 @@ import wikipedia
 
 def answer_wiki(text):
     #get query string
+    question = ['là ai', 'là gì', 'là cái gì']
+    pos = text.find('là ai')
+    i=1
+    while pos == -1:
+        pos = text.find(question[i])
+        i+=1
+    query = text[0:pos-1]
     wikipedia.set_lang('vi')
     try:
-        return wikipedia.summary(text, sentences=1)
+        return wikipedia.summary(query, sentences=1)
     except:
         return 'Jarvis không biết đáp án của câu hỏi'
